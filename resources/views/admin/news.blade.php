7af70fa010a1392a89ecb25e28276a6587b2eff4
Dragomir Dimitrov <vagrant@homestead>
2019-02-25 14:06:17 +0000

    news and papers form

--- resources/views/admin/news.blade.php
+++ resources/views/admin/news.blade.php
@@ -5,7 +5,32 @@
 <div class="card">
     <div class="card-header">Add news</div>
     <div class="card-body">
+        <form method="post">
+            <div class="form-group">
+                <label for="title">Title</label>
+                <input type="text" class="form-control" name="title">
+            </div>
 
+            <div class="form-group">
+                <label for="content">Content</label>
+                <textarea class="form-control" name="content" rows="3"></textarea>
+            </div>
+
+            <div class="form-group">
+                <label for="exampleFormControlSelect1">Add picture</label>
+                <select class="form-control" id="exampleFormControlSelect1">
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                </select>
+            </div>
+
+            {{ csrf_field() }}
+
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
     </div>
 </div>
 @stop
--- resources/views/admin/paper.blade.php
+++ resources/views/admin/paper.blade.php
@@ -5,7 +5,36 @@
 <div class="card">
     <div class="card-header">Add new paper</div>
     <div class="card-body">
+        <form method="post">
+            <div class="form-group">
+                <label for="authors">Authors</label>
+                <input type="text" class="form-control" name="authors">
+            </div>
 
+            <div class="form-group">
+                <label for="year">Year</label>
+                <input type="text" class="form-control" name="year">
+            </div>
+
+            <div class="form-group">
+                <label for="title">Title</label>
+                <input type="text" class="form-control" name="title">
+            </div>
+
+            <div class="form-group">
+                <label for="journal">Journal</label>
+                <input type="text" class="form-control" name="journal">
+            </div>
+
+            <div class="form-group">
+                <label for="file">File location</label>
+                <input type="text" class="form-control" name="file">
+            </div>
+
+            {{ csrf_field() }}
+
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
     </div>
 </div>
 @stop
