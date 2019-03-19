e95a65877bf73a0209d020676a7db7e37a42bb5f
Dragomir Dimitrov <vagrant@homestead>
2019-03-19 12:35:30 +0000

    project description and team

--- resources/views/main/index.blade.php
+++ resources/views/main/index.blade.php
@@ -8,22 +8,98 @@
                 <div class="card-header"><h2>The project</h2></div>
                 <div class="card-body">
                     <p>
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                        In his book "The Republic", the Greek philosopher Plato (428-348 BC) wrote about a wisdom dialogue with Socrates (469-399 BC). In order to give a clearer picture of the world in which the man exists, Plato used the allegory with the cave. According to it, man is like a prisoner in a cave. He sees how the shadows of the things moving behind him in the light of the fire on the walls, and imagines that it is reality. This allegory serves to draw attention to our perception of the world and reality - if we only perceive our surroundings based on what we "see", our experience is limited and we risk becoming acquainted with an incomplete knowledge and even misconceptions about the world around us.
                     </p>
 
                     <p>
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                        This fundamental philosophical example of knowledge can also be projected into biological sciences, where it is often necessary to rethink existing concepts and, based on the development of new technologies, to seek new approaches for solving a number of problems related to the origin of species and the formation of modern flora and fauna, for which we rarely have direct evidence.
                     </p>
 
                     <p>
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                       The soil fauna (cave,  deep-soil and leaf litter) of the Balkans is at least twice as rich as in the rest of the Earth. The distribution of Balkan endemic species and the links between them in the accepted zoo geographic regions of the Balkans, of which two out of five in the territory of Bulgaria, can be used to better understand the processes of species formation and for the identification of the factors that led to it.
+                    </p>
+
+                    <h4>
+                        The research will focus on endemic spiders (Aranea) and beetles (Coleoptera).
+                    </h4>
+
+                    <p>
+                        Bulgaria occupies a very interesting place in Europe in terms of the achievements of classical biological sciences such as taxonomy and faunistics. On the one hand, there is a well-established tradition and a school with a leading role among the other Balkan countries. On the other hand, there are still "white spots" in the studies of many groups of animals. The landscape specifics of Bulgaria show a great potential for the discovery of new species as well as for the environmental protection of known ones. At the same time, our country lags behind in the processes of the digitisation of its scientific literature, the incorporation of data from scientific collections into databases, their use for modern phylogenetic analyses and the provision of free access to the data. There is a lack, (with rare exceptions for particular groups), of targeted molecular research and molecular-phylogenetic reconstructions, which are increasingly applied in modern evolutionary and ecological research.
+                    </p>
+
+                    <p>
+                        With regard to spiders (Araneae), it is assumed that the degree of their exploration in Bulgaria is about 70% (1046 species). They have been unequally studied in taxonomic, faunistic, biogeographic and ecological terms, mainly at morphological level, and the modern molecular-taxonomic and phylogenetic studies are in their early stages.
+                        As far as beetles (Coleoptera) are concerned, it is estimated that the total degree of research in Bulgaria is about 60% (over 6000 species). Many families, as well as a number of areas and ecological niches (for example, soil and cave insects), have been insufficiently studied. At the same time, beetles are one of the groups of animals with the largest species composition and they are of great ecological and economic significance.
+                    </p>
+
+                    <p>
+                        Examining endemic and slowly moving invertebrate soil and cave animals is motivated by their large indicative value in phylogenetic and biogeographic studies. The genera of the selected model groups are considered to be relatively well studied in terms of species composition and the available information on them is a good basis for studying the fundamental processes of the origin and formation of the invertebrate fauna on the Balkan Peninsula. At the same time, they pose many questions and challenges from taxonomic, faunistic, phylogenetic and conservationist point of view. The criteria for choosing the particular genera are as follows:
+                    </p>
+
+                    <ol>
+                        <li>
+                            Having reduced mobility and multiple adaptations for underground life and therefore being isolated spatially and ecologically;
+                        </li>
+                        <li>
+                            Being comprised of relict, neoendemic or polymorphic species, so that their presence in a given region and habitat can show the trends in evolution, adaptation and species formation;
+                        </li>
+                        <li>
+                            Containing species with unclear and controversial taxonomic status, and possibly hidden species.
+                        </li>
+                        <li>
+                            Being very well represented in the soil environment and relatively easy to find. The methods for collecting material are similar, which optimises fieldwork. The research will focus on the following model groups.
+                        </li>
+                    </ol>
+
+                    <h4>The main objective and working hypotheses are:</h4>
+
+                    <ol>
+                        <li>
+                            Our main hypothesis is that the phylogenetic development of the model groups of invertebrates, their relationships and the ways of species formation will be determined more precisely and will be empirically proven by using an integrated, cybertaxonomic approach (obtaining and analysing the data from molecular, statistical and morphological studies, organised and easily accessible in the virtual space).
+                            The new knowledge, incorporated in an on-line cybertaxonomic form, will also allow better protection of these unique species and their specific habitats.
+                        </li>
+
+                        <li>
+                            Applying the methods of cybertaxonomy will increase the quality and quantity of the scientific production in the sphere of invertebrate zoology and will facilitate the usage of the data obtained  – both by scientists and by the general public in Bulgaria.
+                        </li>
+                    </ol>
+
+                    <h4>What is the Cybertaxonomy?</h4>
+
+                    <p>
+                        Cybertaxonomy is an approach to modern analysis and to solving various related problems (rich fauna, insufficient research combined with poor use of new methods of data analysis and organisation, lack of targeted education). It also provides a possibility for a contemporary view on the phylogeny of the fauna of the Balkans.
+                        The objective of this project is to lay the basis of the cybertaxonomic studies into model genera of invertebrate animals, with the perspective of developing a methodology that can be validated and adopted as an updated standard in zoological and phylogenetic studies in Bulgaria and the Balkans.
+                    </p>
+
+                    <p>
+                        Cybertaxonomy, as defined by the European Distributed Institute of Taxonomy (EDIT), is an integrated way of conducting taxonomic research using standardised electronic tools and resources, consisting of: 1) a workflow that involves the use of standardised electronic tools of access to information (databases, electronic publications) and / or that generates new knowledge bases; 2) the process of analysing information that is stored in non-digital sources or is converted from them into a standardised electronic format, and synthesising this information in a digital representation of one or more taxonomic concepts; 3) performing taxonomic work online and through software tools.
+                    </p>
+
+                    <p>
+                        Cybertaxonomy, similarly to traditional taxonomy, is complex. This means that taxonomies combine, synthesise and analyse all available data that are informative about the taxonomic level being investigated. Typical data sources include morphological, molecular, fossil and ontogenetic as well as ethological, physiological, biochemical and other sources of information.
+                    </p>
+
+                    <p>
+                        Cybertaxonomy could be visualised and explained as a GIS-like environment with multiple layers of data in which the geographic data has been replaced by morphological, occurrence data, molecular data, images and sound recordings, and so on. In addition, there are "layers" of algorithms and data processing applications for phylogeny, horology, spatial or environmental relations. Users can activate any combination of "layers" to download the desired information into a multi-layered "network". The possibilities are numerous and diverse: dichotomous or interactive diagnostic keys, catalogues of species in certain areas, distribution, environmental models (location data or predicted ranges based on climate and environment), three-dimensional phenotypic variation visualizations, and many others.
+                    </p>
+
+                    <p>
+                        Another key advantage of this approach is the ability to synchronise and automatically exchange data among different platforms and databases. Technological advances greatly increase the speed of publication, the use of data by stakeholders (scientists, amateurs, administration, business, etc.).
+                    </p>
+
+                    <p>
+                        Cybertaxonomy not only changes the way taxonomists work, moreover, it enhances the ways in which environmentalists can access and use taxonomic data, information and knowledge. The new methods of collecting, structuring and analysing taxonomic and natural history information allow environmentalists to better understand complex ecosystems, and to detect and monitor environmental changes more accurately and more effectively in order to achieve the objectives of sustainable environmental services. At the same time, the use of innovative interdisciplinary approaches attracts the attention of the contemporary generation of students and young scientists.
+                    </p>
+
+                    <p style="font-weight: bold">
+                        The research is supported by the project “Cybertaxonomic approach to phylogenetic studies of model invertebrate genera (Invertebrata, Arachnida, Insecta) clarifyng the problems of origin, formation and conservation of the Invertebrate Fauna of the Balkan Peninsula” (National Science Fund, Ministry of Education, Youth and Science of the Republic of Bulgaria, Grant KP-06-Н21/1-17.12.2018).
                     </p>
                 </div>
             </div>
         </div>
         <div class="col-12 col-md-3">
             <div class="card">
-                <div class="card-header"><h2>News:</h2></div>
+                <div class="card-header"><h2>News</h2></div>
                 <div class="card-body">
                     @foreach($news as $newsItem)
                     <aside style="text-align: left;">
--- resources/views/main/team.blade.php
+++ resources/views/main/team.blade.php
@@ -8,7 +8,45 @@
             <div class="card">
                 <div class="card-header"><h2>The project team</h2></div>
                 <div class="card-body">
-                    <p>Келеши ...</p>
+                    <p>
+                        - <a href="http://www.nmnhs.com/stoev-pavel-en.html" target="_blank">
+                            <span style="font-weight: bold">Professor, Dr. Pavel Stoev</span></a>, an experienced taxonomist and faunist, one of the first scientists used and continue using cybertaxonomic approaches in his taxonomic work. He has participated in numerous international projects related to cybertaxonomy, publishing and dissemination of data.
+                    </p>
+
+                    <p>
+                        - <span style="font-weight: bold">Associate Professor, Dr. Hristo Delchev</span>, a long-term arachnologist, author of numerous publications and monographs related to the spiders of Bulgaria and the Balkans, with special interest in the conservation of species and nature. He maintains the largest spider database in Bulgaria.
+                    </p>
+
+                    <p>
+                        - <a href="http://www.nmnhs.com/bekchiev-rostislav-en.html" target="_blank">
+                            <span style="font-weight: bold">Assistant Professor, Dr. Rostislav Bekchiev</span></a>, a colleopterologist with interests in the taxonomy of the family Staphylindae (mainly Pselaphinae), author and co-author of 21 new taxa from the Balkans, Turkey, Nepal, Japan and others. A participant, key expert and project manager for mapping and monitoring of protected species, management plans and environmental assessments in Bulgaria with a year-long experience in databases (Access, Scratchpads, EndNote, Zotero, etc.)
+                    </p>
+
+                    <p>
+                        - <a href="https://www.uni-sofia.bg/index.php/bul/universitet_t/fakulteti/biologicheski_fakultet2/katedri_i_samostoyatelni_zvena/katedri/zoologiya_i_antropologiya/prepodavateli/gl_as_d_r_rumyana_kostova" target="_blank">
+                            <span style="font-weight: bold">Assistant Professor, Dr. Rumyana Kostova</span></a>, a coleopterologist with a strong interest in the Carabidae family and  a long experience in environmental research, data processing and statistical analysis.
+                    </p>
+
+                    <p>
+                        - <a href="http://www.iber.bas.bg/?q=bg/user/39" target="_blank">
+                            <span style="font-weight: bold">Assistant Professor, Dr. Anelia Bobeva</span></a>, a post-PhD student, a molecular biologist with a intense interest in applying genetic methods in zoology. Aparticipant in numerous projects with proven team work experience and skills.
+                    </p>
+
+                    <p>
+                        - <a href="https://www.researchgate.net/profile/Dragomir_Dimitrov2" target="_blank"><span style="font-weight: bold">Dr. Dragomir Dimitrov</span></a>, an arachnologist with extensive interests and experience in the systematisation of faunistic and bibliographic information and its use in environmental studies. He has professional knowledge in programming, organising databases and innovative ways of presenting and processing information.
+                    </p>
+
+                    <p>
+                        - <span style="font-weight: bold">Simeon Indzhov</span>, a student with a strong interest in arachnology and the desire to learn new methods and technologies. At present he is a Bachelor of Mathematics at Sofia University "St. Kliment Ohridski ".
+                    </p>
+
+                    <p>
+                        - <a href="https://www.researchgate.net/profile/Peter_Hlavac4" target="_blank"><span style="font-weight: bold">Peter Hlaváč</span></a> (Czech University of Life Sciences Prague, Faculty of Forestry and Wood Sciences, Department of Forest Protection and Entomology, Kamýcká 1176, CZ-165 21 Praha 6-Suchdol, Czech Republic) – He has been working in the field of entomology since 1993, having published over 110 articles mainly focused on cave, soil and myrmecophilic Carabidae, Staphylinidae and Curculionidae. He has a special interest in the fauna of the Balkans.
+                    </p>
+
+                    <p>
+                        - <a href="http://www.naturkundemuseum-bw.de/forschung/entomology/faille" target="_blank"><span style="font-weight: bold">Arnaud Faille</span></a> (Coleoptera curator in Stuttgart State Museum of Natural History (Germany) - An entomologist with a special interest in the use of new molecular and morphometric methods in taxonomy and phylogeny. He has an interest in Trechini tribus (Carabidae). Author of over 45 publications.
+                    </p>
                 </div>
             </div>
         </div>
